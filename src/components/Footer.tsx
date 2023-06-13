69ed263dbbdf2af840cc04d03f783818c5d71731
Walker Lobato <walker.b.lobato@gmail.com>
2023-06-13 13:01:10 -0300

    Finishing project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <Center backgroundColor='#1e1e1e' color='white' width='100%' >
       <Text padding='15px' textDecoration='none'>
-        Aplicação Desenvolvida por <Link href='https://github.com/WalkerBrum' color='teal.500'  fontWeight='bold' isExternal>Walker Lobato</Link>
+        Aplicação Desenvolvida por <Link href='https://github.com/WalkerBrum/dio-bank-react' color='teal.500'  fontWeight='bold' isExternal>Walker Lobato</Link>
       </Text>
     </Center>
   )
